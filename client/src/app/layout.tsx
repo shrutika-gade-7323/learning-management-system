ed3b43f4e1feac8c58bcb7bbfdcb92f50b55690f
shrutika-gade-7323 <shrutika.github@gmail.com>
2024-12-03 14:53:42 +0530

    Frontend Installation

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${dmSans.className}`}>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
